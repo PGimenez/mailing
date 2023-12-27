8bdb1817fbf9c0983d242fe801fd56d5e8bd8052
Pere Giménez <gimenezfebrer@gmail.com>
2023-12-27 14:10:27 +0100

    logging

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM rust:latest
 WORKDIR /app
 COPY mailing/target/release/mailing /app/
 EXPOSE 8080
+ENV RUST_LOG=info
 CMD ["./mailing"]
--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,34 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use log::{info, warn};
+use serde::Deserialize;
 use std::net::TcpListener;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
+    info!("Greeting request for name: {}", &name); // Log message
     format!("Hello {}!", &name)
 }
 
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    info!("Subscribing email: {} with name: {}", form.email, form.name); // Log message
     HttpResponse::Ok().finish()
 }
 
 async fn health_check() -> impl Responder {
+    info!("Health check hit"); // Log message
     HttpResponse::Ok()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info")); // Initialize the logger
+
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
@@ -31,5 +38,8 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     })
     .listen(listener)?
     .run();
+
     Ok(server)
 }
+
+// Your main function here
